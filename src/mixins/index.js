3c469f0218208f61ce571f1fbc927a3d35e7d1bc
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-08-30 20:53:52 +0200

    Show daily average only when on mainnet

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -99,9 +99,10 @@ const methods = {
     }).format(value)
   },
 
-  readableCurrency(value, showDecimals = true, currency = null, normalise = true) {
+  readableCurrency(value, showDecimals = true, rate = null, currency = null, normalise = true) {
     const currencyName = currency || store.getters['currency/name']
-    value *= store.getters['currency/rate']
+
+    value *= rate || store.getters['currency/rate']
 
     if (normalise) {
       value /= Math.pow(10, 8)
