7b1f15e4501ddbd16de2b27e24b2acbda5edaefd
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2018-09-21 04:33:47 +0200

    fix: tooltip currency
    * fix: always show tooltip currencies with 2 decimals
    * test: add tests for currency decimals
    * refactor: use toLocaleString to properly format currency

--- test/unit/specs/mixins/readable-currency.spec.js
+++ test/unit/specs/mixins/readable-currency.spec.js
@@ -1,13 +1,28 @@
 import mixins from '@/mixins'
 
+const displayCurrency = function(value) {
+  return value.toLocaleString('en', {
+    style: 'currency',
+    currency: 'eur'
+  })
+}
+
 describe('readable currency mixin', () => {
   it('should properly format the given data', () => {
-    expect(mixins.readableCurrency(100000000)).toEqual('1.00')
-    expect(mixins.readableCurrency(1000000000)).toEqual('10.00')
-    expect(mixins.readableCurrency(10000000000)).toEqual('100.00')
-    expect(mixins.readableCurrency(100000000000)).toEqual(Number(1000).toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
+    expect(mixins.readableCurrency(100000000, false, null, 'BTC')).toEqual('1')
+    expect(mixins.readableCurrency(1000000000, false, null, 'BTC')).toEqual('10')
+    expect(mixins.readableCurrency(10000000000, false, null, 'BTC')).toEqual('100')
+    expect(mixins.readableCurrency(100000000000, false, null, 'BTC')).toEqual(Number(1000).toLocaleString())
+    expect(mixins.readableCurrency(100000000000, true, null, 'BTC')).toEqual(Number(1000).toLocaleString(undefined, {
+        minimumFractionDigits: 8,
+        maximumFractionDigits: 8,
       }))
+  })
+
+  it('should format currency with 2 decimals', () => {
+    expect(mixins.readableCurrency(10, true, 1, 'eur', false)).toEqual(displayCurrency(10))
+    expect(mixins.readableCurrency(10.3, true, 1, 'eur', false)).toEqual(displayCurrency(10.30))
+    expect(mixins.readableCurrency(10.34, true, 1, 'eur', false)).toEqual(displayCurrency(10.34))
+    expect(mixins.readableCurrency(10.349, true, 1, 'eur', false)).toEqual(displayCurrency(10.35))
   })
 })
